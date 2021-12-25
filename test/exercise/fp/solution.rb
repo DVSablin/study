cd97fb4f476dcd9e12e75b60d47ad8fb32b44fcf
Dmitry Sablin <sablindevelopment@gmail.com>
2021-12-26 03:46:01 +0400

    rating exercise solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,10 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        all_names = array.select { |item| item['country'].to_s.split(',').to_a.length >= 2 && item['rating_kinopoisk'].to_f.positive? }
+                         .map { |n| n['rating_kinopoisk'].to_f }
+        all_names.reduce(:+) / all_names.length
       end
 
       def chars_count(_films, _threshold)
